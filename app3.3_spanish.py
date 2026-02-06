a919cae6322de3fcb7be3d585ee566f32c6d5ceb
Robert Hernández <robert@actuariayfinanzas.net>
2026-02-06 10:00:07 -0600

    Add GitHub link to sidebar in Spanish app

--- app3.3_spanish.py
+++ app3.3_spanish.py
@@ -294,7 +294,12 @@ with st.sidebar:
         </a>
         <a href="https://www.credly.com/users/robert-hernandez.89bffe7b" class="sidebar-link">
             🏆 Credentials
+
         </a>
+        <a href="https://github.com/robert0777" class="sidebar-link">
+        🐙 GitHub
+        </a>
+        
         <a href="mailto:robert@actuariayfinanzas.net" class="sidebar-link">
             📧 Contact
         </a>
